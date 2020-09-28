3ce1a67a09fd8c3029f78e3c3ba3e9f945e2fb5c
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-28 18:32:16 +0000

    Automatically backed up by Learn

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -9,4 +9,10 @@ class MusicImporter
     @files || Dir.entries(@path).select! {|entry| entry.end_with?(".mp3")}
   end
 
+  def import
+    self.files.each do |filename|
+      Song.create_from_filename(filename)
+    end
+  end 
+
 end
