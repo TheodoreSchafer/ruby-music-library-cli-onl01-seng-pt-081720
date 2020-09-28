164b5ef8bffe0863edc23d035580c7fe3bfcf8bf
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-28 18:35:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -66,7 +66,7 @@ class Song
 
     artist = Artist.find_or_create_by_name(artist)
     genre = Genre.find_or_create_by_name(genre)
-    self.new(song_name)
+    self.new(song_name, artist, genre)
 
 
  end
