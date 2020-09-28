148fdb879e74a304cfa12cf1a77de3a07fd95b29
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-28 18:16:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -61,22 +61,17 @@ class Song
     self.find_by_name(name) || self.create(name)
   end
 
-  def self.new_from_filename(name)
-    array = filename.split(" - ")
-
-    song_name = array[1]
-    artist_name = array[0]
-    genre_name = array[2].split(".mp3").join
-
-
-    artist = Artist.find_or_create_by_name(artist_name)
-    genre = Genre.find_or_create_by_name(genre_name)
-    self.new(song_name, artist, genre)
-  end
-
-  def self.create_from_filename(filename)
-   self.new_from_filename(filename).save
+  def self.new_from_filename(filename)
+   title = filename.split(" - ")
+   artist = Artist.find_or_create_by_name(title[0])
+   genre = Genre.find_or_create_by_name(title[2][0..-5])
+   create(title[1], artist, genre)
  end
+
+ def self.create_from_filename(filename)
+   new_from_filename(filename)
+ end
+
 
 
 end
